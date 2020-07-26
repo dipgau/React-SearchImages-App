07a7b9fd8bf089c96f1a2c837c3d125177e428d5
dipgau <e1701299@edu.vamk.fi>
2020-07-26 11:18:30 +0300

    Updates

---
+++ precache-manifest.81dfec7bae103c2d999a948f7049d6e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbb762005bdc79a1aaeb57f106996f7a",
+    "url": "/React-SearchImages-App/index.html"
+  },
+  {
+    "revision": "f75dacb41c32dc33adbb",
+    "url": "/React-SearchImages-App/static/css/main.5678862c.chunk.css"
+  },
+  {
+    "revision": "1702e95cef7ad9166abf",
+    "url": "/React-SearchImages-App/static/js/2.9fa9bafe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-SearchImages-App/static/js/2.9fa9bafe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f75dacb41c32dc33adbb",
+    "url": "/React-SearchImages-App/static/js/main.c8d0bc34.chunk.js"
+  },
+  {
+    "revision": "7bb9b51b22575f839e40",
+    "url": "/React-SearchImages-App/static/js/runtime-main.df6c908a.js"
+  }
+]);
